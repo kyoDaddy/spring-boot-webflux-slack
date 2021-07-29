66d8dc75e1a36133bd3010c61b112837b2e60048
KYO <rlarbghrbgh@gmail.com>
2021-07-29 14:49:24 +0900

    WebTestClient 사용하여 테스트케이스 작성해봄

--- src/main/java/com/demo/process/controller/TestController.java
+++ src/main/java/com/demo/process/controller/TestController.java
@@ -4,13 +4,16 @@ import com.demo.config.prop.SlackProp;
 import com.demo.process.dto.TimeZoneDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.server.reactive.ServerHttpRequest;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import java.sql.Time;
 import java.time.*;
 import java.util.Arrays;
 import java.util.List;
